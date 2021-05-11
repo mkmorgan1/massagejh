e6b2eca3b37b879b46c52dbe96c432596d4c6a5a
Matthew <m_kmorgan@hotmail.com>
2021-05-10 20:57:05 -0600

    created a webpack that compile jsx, js, and scss files

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TempApp from './temp/TempApp.jsx';
+
+// ReactDOM.render(<h1>what </h1>, document.getElementById('app'));
+ReactDOM.render(<TempApp />, document.getElementById('app'));
---
+++ client/temp/TempApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './tempStyles.module.scss';
+
+const TempApp = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Coming Soon</h1>
+    </>
+  );
+}
+
+export default TempApp;
