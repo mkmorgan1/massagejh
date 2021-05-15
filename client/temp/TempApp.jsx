f9cb4cebd5f6fb13a97ac3f9d5e04a767af932d9
Matthew <m_kmorgan@hotmail.com>
2021-05-15 10:36:28 -0600

    added some style and a footer for contact

--- client/temp/TempApp.jsx
+++ client/temp/TempApp.jsx
@@ -7,6 +7,10 @@ const TempApp = () => {
       <div className={styles.circle}>
         <h1 className={styles.title}>Coming Soon!!</h1>
       </div>
+      <hr/>
+      <div className={styles.contact}>
+        <p>Contact: <a href="mailto:mkmorgan1994@gmail.com">Email</a>, <a href="tel:208-709-5531">Phone</a></p>
+      </div>
     </>
   );
 }
