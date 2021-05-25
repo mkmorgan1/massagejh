c44bdfd00a55132e1408a95d11315f4c1fa74a27
Matthew <m_kmorgan@hotmail.com>
2021-05-24 22:06:14 -0600

    chaged styles from module to work with motion ui series

--- client/temp/TempApp.jsx
+++ client/temp/TempApp.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import styles from './tempStyles.module.scss';
+import React, { useEffect } from 'react';
+import './tempStyles.scss';
+import $ from 'jquery';
 
 const TempApp = () => {
+
+  // useEffect(()=> {
+  //   $('#contact').addClass('is-animating');
+  // },[]);
   return (
     <>
-      <div className={styles.circle}>
-        <h1 className={styles.title}>Coming Soon!!</h1>
-      </div>
-      <hr/>
-      <div className={styles.contact}>
-        <p className={styles.contactTitle}>Contact: </p>
-          <a className={styles.email} href="mailto:mkmorgan1994@gmail.com">Email, </a>
-          <a className={styles.phone} href="tel:208-709-5531">Phone</a>
+      <div className='is-animating'>
+        <h1 className='mainTitle'>Massage JH</h1>
+        <div className='circle'>
+          <h1 className='title'>Coming Soon!!</h1>
+        </div>
+        <hr/>
+        <div id='contact'>
+          <p className='contactTitle'>Contact: </p>
+          <a className='email' href="mailto:mkmorgan1994@gmail.com">Email</a>
+          <a className='phone' href="tel:208-709-5531">Phone</a>
+        </div>
       </div>
     </>
   );
